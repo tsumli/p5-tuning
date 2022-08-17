3344d3a47c120c4163de974d00fcaff5feb9a1c5
tsumli <sample@example.com>
2022-08-18 00:34:12 +0900

    add: connection between frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 
 @app.get("/")
@@ -11,7 +19,8 @@ async def root():
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
-        host="127.0.0.1",
-        port=8080
+        "main:app",
+        host="0.0.0.0",
+        port=8080,
+        reload=True,
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,36 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import axios from "axios"
+
+
+const App = (props) => {
+    const [data, setData] = React.useState(null);
+    const axiosBase = axios.create(
+        {
+            baseURL: 'http://0.0.0.0:8080',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+            responseType: 'json'  
+        }
+    );
+
+    React.useEffect(() => {
+        axiosBase.get(
+            '/', { params: {}}
+        ).then(response => {
+            setData(response.data.message)
+        }).catch(error => {
+            setData("Error")
+            console.log(error)
+        })
+    }, []);
+
+    return (
+        <div> {data} </div>
+    )
+}
+
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
-
-function App() {
-  return <h1>Hello, world!</h1>;
-}
